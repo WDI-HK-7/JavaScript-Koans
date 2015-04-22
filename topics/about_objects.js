0f08d830cf725345bd3f3b69893e98a57e209eba
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-04-22 10:27:57 +0800

    42 assertions of 109 passed

--- topics/about_objects.js
+++ topics/about_objects.js
@@ -3,13 +3,13 @@ module("About Objects (topics/about_objects.js)");
 
 test("object type", function() {
     var empty_object = {};
-    equal(__, typeof(empty_object), 'what is the type of an object?');
+    equal("object", typeof(empty_object), 'what is the type of an object?');
 });
 
 test("object literal notation", function() {
     var person = {
-        __:__,
-        __:__
+        name: "Amory Blaine",
+        age: 102
     };
     equal("Amory Blaine", person.name, "what is the person's name?");
     equal(102, person.age, "what is the person's age?");
